47d495ececbb5f9d5c67fcdfc743f981a06b642c
Rahul Suram <rahul.suram@zemosolabs.com>
2024-11-21 16:13:06 +0530

    commit after mvn build7

--- src/test/java/com/example/CheckrApplication/CheckrApplicationTests.java
+++ src/test/java/com/example/CheckrApplication/CheckrApplicationTests.java
@@ -1,13 +1,53 @@
 package com.example.CheckrApplication;
 
+import com.example.CheckrApplication.JPARepository.CandidateRepository;
+import com.example.CheckrApplication.JPARepository.ReportRepository;
+import com.example.CheckrApplication.JPARepository.UserRepository;
+import com.example.CheckrApplication.controller.AuthenticationController;
+import com.example.CheckrApplication.controller.CandidateController;
+import com.example.CheckrApplication.service.AuthenticationService;
+import com.example.CheckrApplication.service.CandidateService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CheckrApplicationTests {
 
+	@Autowired
+	private AuthenticationController authenticationController;
+
+	@Autowired
+	private CandidateController candidateController;
+
+	@Autowired
+	private CandidateRepository candidateRepository;
+
+	@Autowired
+	private ReportRepository reportRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private AuthenticationService authenticationService;
+
+	@Autowired
+	private CandidateService candidateService;
+
+
 	@Test
 	void contextLoads() {
+		assertThat(authenticationController).isNotNull();
+		assertThat(candidateController).isNotNull();
+		assertThat(authenticationService).isNotNull();
+		assertThat(candidateService).isNotNull();
+
+		assertThat(candidateRepository).isNotNull();
+		assertThat(reportRepository).isNotNull();
+		assertThat(userRepository).isNotNull();
 	}
 
 }
